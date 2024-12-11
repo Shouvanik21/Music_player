e7fe2fdacc7ff333704ee27aec7ffaee1141f587
Shouvanik21 <shouvanik210305@gmail.com>
2024-12-12 00:18:41 +0530

    updated commit

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,9 @@ const playpause_btn=document.querySelector(".playpause-btn");
 const prev_btn=document.querySelector(".prev-btn");
 const next_btn=document.querySelector(".next-btn");
 
-const seek_slider=document.querySelector(".now-playing");
-const curr_time=document.querySelector(".now-playing");
-const total_duration=document.querySelector(".now-playing");
+const seek_slider=document.querySelector(".seek-slider");
+const curr_time=document.querySelector(".current-time");
+const total_duration=document.querySelector(".total-duration");
 const randomIcon=document.querySelector(".ri-shuffle-line");
 const curr_track=document.createElement("audio");
 
@@ -17,33 +17,6 @@ let track_index=0;
 let isPlaying=false;
 let isRandom=false;
 let updateTimer;
-
-const music_list=[
-    {
-        img:"./media/mediapic/img1.jpg",
-        name:"One Of The Girls",
-        artist:"Jennie Kim, Lily-Rose Depp, and The Weeknd",
-        music:"./media/mediasong/One-Of-The-Girls.mp3"
-    },
-    {
-        img:"./media/mediapic/img2.jpg",
-        name:"Heat Waves",
-        artist:"Glass Animals",
-        music:"./media/mediasong/Heat-Waves.mp3"
-    },
-    {
-        img:"./media/mediapic/img3.jpg",
-        name:"Summertime Sadness",
-        artist:"Lana Del Rey",
-        music:"./media/mediasong/Summertime-Sadness.mp3"
-    },
-    {
-        img:"./media/mediapic/img4.jpg",
-        name:"Best of Me",
-        artist:"NEFFEX",
-        music:"./media/mediasong/NEFFEX - Best of Me.mp3"
-    }
-];
 
 loadTrack(track_index);
 
@@ -58,5 +31,13 @@ function loadTrack(track_index){
     track_name.innerHTML=music_list[track_index].name;
     track_artist.innerHTML=music_list[track_index].artist;
 
-    
+    updateTimer=setInterval(setUpdate,1000);
+
+    curr_track.addEventListener("ended",nextTrack);
+}
+
+function reset(){
+    curr_time.innerHTML="00:00";
+    total_duration.innerHTML="00:00";
+    seek_slider.value=0;
 }
