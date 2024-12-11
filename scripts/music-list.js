3fcadf000eb055a14c6a9b64f5bcd8cf99157405
Shouvanik21 <shouvanik210305@gmail.com>
2024-12-12 01:32:32 +0530

    updated commit

--- scripts/music-list.js
+++ scripts/music-list.js
@@ -1,26 +1,38 @@
 const music_list=[
     {
-        img:"./media/mediapic/img1.jpg",
+        img:"./media/pic/img1.jpg",
         name:"One Of The Girls",
         artist:"Jennie Kim, Lily-Rose Depp, and The Weeknd",
-        music:"./media/mediasong/One-Of-The-Girls.mp3"
+        music:"./media/song/One-Of-The-Girls.mp3"
     },
     {
-        img:"./media/mediapic/img2.jpg",
+        img:"./media/pic/img2.jpg",
         name:"Heat Waves",
         artist:"Glass Animals",
-        music:"./media/mediasong/Heat-Waves.mp3"
+        music:"./media/song/Heat-Waves.mp3"
     },
     {
-        img:"./media/mediapic/img3.jpg",
+        img:"./media/pic/img3.jpg",
         name:"Summertime Sadness",
         artist:"Lana Del Rey",
-        music:"./media/mediasong/Summertime-Sadness.mp3"
+        music:"./media/song/Summertime-Sadness.mp3"
     },
     {
-        img:"./media/mediapic/img4.jpg",
+        img:"./media/pic/img4.jpg",
         name:"Best of Me",
         artist:"NEFFEX",
-        music:"./media/mediasong/NEFFEX - Best of Me.mp3"
+        music:"./media/song/NEFFEX - Best of Me.mp3"
+    },
+    {
+        img:"./media/pic/img5.jpg",
+        name:"Moth to a Flame",
+        artist:"Swedish House Mafia and The Weeknd",
+        music:"./media/song/Swedish House Mafia and The Weeknd - Moth To A Flame.mp3"
+    },
+    {
+        img:"./media/pic/img6.jpg",
+        name:"I Wanna Be Yours",
+        artist:"Arctic Monkeys",
+        music:"./media/song/Arctic Monkeys - I Wanna Be Yours.mp3"
     }
 ];
